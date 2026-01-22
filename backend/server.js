c7b27876950dee8a8bd45b5a7e9276ca12c4bdb5
ansatmadagascar <antsamadagascar@gmail.com>
2026-01-22 13:50:30 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
+require('dotenv').config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// ✅ Autoriser CORS
-app.use(cors({
-  origin: 'http://localhost:4200', // Angular
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
-}));
-
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-// Routes
-const articleRoutes = require('./routes/articles');
-app.use('/articles', articleRoutes);
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log(" MongoDB connecté"))
+  .catch(err => console.log(" Erreur MongoDB:", err));
 
-app.listen(5000, () => {
-  console.log('Serveur démarré sur http://localhost:5000');
-});
+//Routes 
+app.use('/articles', require('./routes/articleRoutes'));
+app.listen(PORT, () => console.log(`Serveur démarré sur le port
+${PORT}`));
